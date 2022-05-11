9bf45c0f82f0698b4680591fd010937762119b60
OneRain233 <onerain233@outlook.com>
2022-05-11 14:13:46 +0800

    Add SDL2 install shell

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 EXE = bin/main
 SRC = src/main.c src/visualization.c src/routeFinding.c
 LINKS = -lSDL2 -lui
-INCDIR = -I/usr/include/SDL2
+INCDIR = -I/usr/local/include/SDL2
 all:
 	gcc -o $(EXE) $(SRC) $(LINKS) $(INCDIR) && pwd && ./$(EXE) "test"
 
--- SDL2_install.sh
+++ SDL2_install.sh
@@ -1,20 +1,51 @@
 #!/bin/sh
 
-# This script will install SDL2
+SDL2_FLAG=0
+LIBUI_FLAG=0
+# This script will install SDL2 and libui for the current user.
 # check if SDL2 is already installed
-if [ -d /usr/include/SDL2 ]; then
+if [ -d /usr/local/include/SDL2 ]; then
   echo "SDL2 is already installed"
-  exit 0
+  SDL2_FLAG=1
 fi
 
 # install SDL2
-echo "Installing SDL2"
-wget -O SDL2.tar.gz https://www.libsdl.org/release/SDL2-2.0.8.tar.gz
-tar -xzf SDL2.tar.gz
+if [ $SDL2_FLAG = 0 ]; then
+  echo "Installing SDL2"
+  wget -O SDL2.tar.gz https://www.libsdl.org/release/SDL2-2.0.8.tar.gz
+  tar -xzf SDL2.tar.gz
 
-cd SDL2-2.0.8 || exit
-./configure --prefix=/usr/local
-make
-make install
+  cd SDL2-2.0.8 || exit
+  ./configure --prefix=/usr/local
+  make
+  make install
+  cd .. || exit
+  rm -rf SDL2-2.0.8
+  rm SDL2.tar.gz
+fi
 
 
+
+#check if libuir is already installed
+if [ -d /usr/local/include/ui.h ]; then
+  echo "libui is already installed"
+  LIBUI_FLAG=1
+fi
+
+if [ $LIBUI_FLAG = 0 ]; then
+
+  echo "Installing libui"
+  git clone https://github.com/andlabs/libui.git
+  cd libui || exit
+  meson setup build
+  ninja -C build
+  cd build/meson-out
+  ln -sf libui.so.0 libui.so
+  cd ../.. || exit
+  install build/meson-out/libui.so /usr/lib/
+  install build/meson-out/libui.so.0 /usr/lib/
+  install ui.h /usr/include/
+  install ui_unix.h /usr/include/
+  cd .. || exit
+  #rm -rf libui
+fi
